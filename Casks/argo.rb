83bfa729d925a43218cebee23f590cc1239489b5
krystal <liaojingyu@xiaohongshu.com>
2026-06-22 23:50:01 +0800

    bump argo to 1.0.6

--- Casks/argo.rb
+++ Casks/argo.rb
@@ -1,6 +1,6 @@
 cask "argo" do
-  version "1.0.5"
-  sha256 "e1a231d161efd46f70141bae9b78579489c5381419832232611a4bef738e7797"
+  version "1.0.6"
+  sha256 "03813ca9647dda9c9ed1718c05c8021597e1ec6af9120731c456ac2fcdd4bb1f"
 
   url "https://github.com/krystal1110/Argo/releases/download/v#{version}/Argo-#{version}.dmg"
   name "Argo"
